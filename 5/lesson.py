d4d035ac400c0b7ebc0448f8d3f69eb39de972e8
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-08-29 20:09:13 +0300

    семинар 5 закончен

---
+++ 5/lesson.py
@@ -0,0 +1,147 @@
+# # База знаний о родственных связях
+
+# father_of = {
+#     ('John', 'Alice'),
+#     ('John', 'Bob'),
+#     ('Michael', 'John'),
+#     ('Michael', 'Sara')
+# }
+
+# def uncle(x, y):
+#     return any((father, y) in father_of and (father, x) in father_of for father in father_of if (x, y) != father)
+
+# person_x = 'Alice'
+# person_y = 'Sara'
+
+# if uncle(person_x, person_y):
+#     print(f"{person_x} - дядя {person_y}")
+# else:
+#     print(f"{person_x} - не дядя {person_y}")
+
+
+    # База знаний об оценках студентов
+
+# grades = {
+#     'Alice': [9, 8, 7, 9, 9],
+#     'Bob': [6, 7, 6, 5, 7],
+#     'Eve': [8, 9, 7, 8, 9],
+#     'Mike': [5, 6, 4, 3, 5]
+# }
+
+# def excellent_student(student):
+#     return sum(grades[student]) / len(grades[student]) > 8
+
+# def struggling_student(student):
+#     return sum(grades[student]) / len(grades[student]) < 5
+
+# students = ['Alice', 'Bob', 'Eve', 'Mike']
+
+# for student in students:
+#     if excellent_student(student):
+#         print(f"{student} - отличник")
+#     elif struggling_student(student):
+#         print(f"{student} - нуждается в академической помощи")
+#     else:
+#         print(f"{student} - средний студент")
+
+# Задача: Напишем программу, которая проверяет, является ли заданное слово палиндромом.
+
+# slovo=str(input("Введите слово: "))
+# if slovo==slovo[::-1]:
+#     print("Poly")
+# else:
+#     print("Not poly")
+
+#     Пусть у нас есть следующий текст:
+
+# "Python is a powerful programming language. Python is used for web development, data analysis, machine learning, and more."
+# Мы хотим подсчитать частоту встречаемости каждого слова в этом тексте.
+
+# s="Python is a powerful programming language. Python is used for web development, data analysis, machine learning, and more.".lower()
+# s=s.replace(",", "")
+# s=s.replace(".", "")
+# sp=s.split()
+# lib={}
+# for i in sp:
+#     if i in lib:
+#         lib[i]+=1
+#     else:
+#         lib[i]=1
+# print(lib)
+
+# Задача: Напишем программу для анализа текста и определения самых часто встречающихся слов в нем.
+
+# s="Python is a powerful programming language. Python is used for web development, data analysis, machine learning, and more.".lower()
+# s=s.replace(",", "")
+# s=s.replace(".", "")
+# sp=s.split()
+# lib={}
+# for i in sp:
+#     if i in lib:
+#         lib[i]+=1
+#     else:
+#         lib[i]=1
+# lib2 = sorted(lib.items(), key = lambda item: item[1], reverse=True)
+# print(lib2)
+
+# Задача: Напишем программу, которая анализирует текст, определяет наиболее часто встречающиеся биграммы (пары последовательных слов) в нем.
+
+# str = "Machine learning is a subfield of artificial intelligence that focuses on the interaction between computers and humans. Machine learning involves the use of algorithms and statistical models to enable computers to perform tasks without explicit programming. In machine learning, data plays a crucial role. Data is used to train the algorithms and improve their performance over time. Machine learning has applications in various fields, including natural language processing, image recognition, and autonomous vehicles."
+# # Приводим текст к нижнему регистру
+# str = str.lower()
+
+# # Убираем точки и запятые
+# str = str.replace(".", "")
+# str = str.replace(",", "")
+
+# # Разбиваем текст на слова
+# sp = str.split()
+
+# # Создаем словарь для хранения пар слов и их количества
+# lib = {}
+
+# # Проходим по списку слов с шагом 1, чтобы формировать биграммы
+# for i in range(len(sp) - 1):
+#     # Формируем биграмму из текущего слова и следующего слова
+#     word_pair = (sp[i], sp[i + 1])
+
+#     # Если биграмма уже есть в словаре, увеличиваем счетчик
+#     if word_pair in lib:
+#         lib[word_pair] += 1
+#     else:
+#         # Если биграммы нет в словаре, добавляем ее и устанавливаем счетчик в 1
+#         lib[word_pair] = 1
+
+# # Сортируем словарь по значению (количеству повторений) в обратном порядке
+# lib2 = sorted(lib.items(), key=lambda item: item[1], reverse=True)
+
+# # Выводим отсортированный список биграмм и их количества
+# print(lib2)
+
+# Препод
+
+# Заданный текст
+text = "Machine learning is a subfield of artificial intelligence that focuses on the interaction between computers and humans. Machine learning involves the use of algorithms and statistical models to enable computers to perform tasks without explicit programming. In machine learning, data plays a crucial role. Data is used to train the algorithms and improve their performance over time. Machine learning has applications in various fields, including natural language processing, image recognition, and autonomous vehicles."
+
+# Разделяем текст на слова, приводим к нижнему регистру и получаем список слов
+words = text.lower().split()
+
+# Создаем словарь для хранения пар слов и их количества
+word_pairs = {}
+
+# Проходим по списку слов с шагом 2, чтобы образовать биграммы
+for i in range(len(words) - 1):
+    # Формируем биграмму из текущего слова и следующего слова
+    word_pair = (words[i], words[i + 1])
+    # Если биграмма уже есть в словаре, увеличиваем счетчик
+    if word_pair in word_pairs:
+        word_pairs[word_pair] += 1
+    else:
+        # Если биграммы нет в словаре, добавляем ее и устанавливаем счетчик в 1
+        word_pairs[word_pair] = 1
+
+# Проходим по словарю биграмм и их количества
+for word_pair, count in word_pairs.items():
+    # Если количество больше 2, выводим биграмму и количество
+    if count > 2:
+        print(f"Word pair: {word_pair}, Count: {count}")
